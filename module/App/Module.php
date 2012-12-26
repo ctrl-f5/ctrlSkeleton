d369695b02a3b7cbc585c212a73255e1f7f9b776
ctrl-f5 <mythix.dev@gmail.com>
2012-12-26 23:18:05 +0100

    removed Namespace subdirectory

--- module/App/Module.php
+++ module/App/Module.php
@@ -88,7 +88,7 @@ class Module
         return array(
             'Zend\Loader\StandardAutoloader' => array(
                 'namespaces' => array(
-                    __NAMESPACE__ => __DIR__.'/src/'.__NAMESPACE__.'/',
+                    __NAMESPACE__ => __DIR__.'/src/',
                 ),
             ),
         );
